737eb93617e0620ea657dbaf956e6ee7b299ab89
godu <arthur.weber@viacesi.fr>
2017-12-05 00:10:40 +0100

    Day 4

--- AdventOfCode/Day2.fs
+++ AdventOfCode/Day2.fs
@@ -1,10 +1,10 @@
 namespace AdventOfCode
 
 module Day2 =
-    let split (separator: string) (s: string): string array = s.Split(separator)
+    let split (sep: char) (s: string): string[] = s.Split [|sep|]
     let parseInt: string -> int = (int)
 
     let answer (puzzle: string): int =
         puzzle
-        |> (split "\n" >> Array.map (split "\t" >> Array.map parseInt))
-        |> Array.sumBy ((fun v -> (Array.min v, Array.max v)) >> (fun (min, max) -> max - min))
+        |> (split '\n' >> Seq.map (split '\t' >> Seq.map parseInt))
+        |> Seq.sumBy ((fun v -> (Seq.min v, Seq.max v)) >> (fun (min, max) -> max - min))
